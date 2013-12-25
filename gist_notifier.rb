3de9e82bf67aad26f5f6355196dfdd804331f9d3
Kramer Campbell <kramer@kramerc.com>
2013-12-24 16:41:13 -0800

    Indent the HTML body.

--- gist_notifier.rb
+++ gist_notifier.rb
@@ -88,15 +88,15 @@ loop do # Keep looping until we run out of pages.
         end
 
         html_body = <<-EOF
-  #{ActionView::Base.new.simple_format(comment.body)}
-  <p style="font-size:small;-webkit-text-size-adjust:none;color:#666;">
-    &mdash;<br>Reply to this on
-    <a href='https://gist.github.com/#{gist.user.login}/#{gist.id}#comment-#{comment.id}'>GitHub Gist</a>.<br>
-    This was sent by
-    <a href="https://github.com/kramerc/gist_notifier">Gist Notifier</a>
-    from host #{Socket.gethostname}. This is not an official GitHub email.
-  </p>
-  EOF
+          #{ActionView::Base.new.simple_format(comment.body)}
+          <p style="font-size:small;-webkit-text-size-adjust:none;color:#666;">
+            &mdash;<br>Reply to this on
+            <a href='https://gist.github.com/#{gist.user.login}/#{gist.id}#comment-#{comment.id}'>GitHub Gist</a>.<br>
+            This was sent by
+            <a href="https://github.com/kramerc/gist_notifier">Gist Notifier</a>
+            from host #{Socket.gethostname}. This is not an official GitHub email.
+          </p>
+        EOF
 
         Pony.mail(config[:mail].merge(
           subject: "Re: [Gist] #{gist_name}",
